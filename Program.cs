98eb5d60e69df7f520be4d94ffaf38d52ab68f14
Jamie Phillips <cphill11@gmail.com>
2020-04-26 20:24:57 -0400

    Basic calculation.
    Changing names.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace fibcalculator
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace fibcalculator {
+    public class Fibonacci {
+        private int number;
+
+        public int Calculate() => Calculate(number);
+        
+        public Fibonacci(int number) {
+            this.number = number;
+        }
+
+        private int Calculate(int number) {
+            if (number < 2) {
+                return number;
+            }
+            return (Calculate(number - 2) + Calculate(number - 1));
+        }
+
+    }
+    
+    class Program {
+        static void Main(string[] args) {
+            var fib = new Fibonacci(4);
         }
     }
 }
